cb60dd56aefa19e8ae0cb3967ccd02c15c11f692
ms143014 <664089244@qq.com>
2020-11-25 23:49:39 +0800

    simpe vue start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+class Compile{
+    el;
+    vm;
+    constructor(args){
+        Object.assign(this, args);
+        const {el} = args;
+        if(typeof el === 'string'){
+            this.el = document.querySelector(el);
+        }else if(el.nodeType === 1){
+
+        }
+    }
+}
+
 class Vue{
-    
+    $el;
+    constructor(args){
+        Object.assign(this, args);
+        const {$el} = this;
+        if($el){
+            window.compile = new Compile({el: $el, vm: this})
+        }
+    }
 }
